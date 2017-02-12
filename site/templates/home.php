a96f27259e49890047c52f537c597170919532a1
Tom <tom@studiographene.com>
2017-02-12 23:10:03 +0000

    added background image to home jumbo

--- site/templates/home.php
+++ site/templates/home.php
@@ -2,7 +2,7 @@
 
   <main class="main" role="main">
     
-    <div class="jumbotron jumbotron-fluid">
+    <div class="jumbotron jumbotron-fluid" id="home-jumbo">
       <div class="container">
         <h1 class="display-3">Fluid jumbotron</h1>
         <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
