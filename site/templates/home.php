48fccbd4e2024942722de9c6a7c2c295da17ddac
Tom <tom@studiographene.com>
2017-03-26 18:36:05 +0100

    Continued work on homepage

--- site/templates/home.php
+++ site/templates/home.php
@@ -14,21 +14,21 @@
           </div>
           <div class="row info-boxes">
             <div class="col">
-              <a href="<?= $pages->findByURI('products')->url() ?>">
+              <a href="<?= $pages->findByURI('about')->url() ?>">
                 <div id="story" class="info-box">
                   Our Story
                 </div>
               </a>
             </div>
             <div class="col">
-              <a>
+              <a href="<?= $pages->findByURI('products')->url() ?>" >
                 <div id="products" class="info-box">
                   Our Products
                 </div>
               </a>
             </div>
             <div class="col">
-              <a href="www.google.com">
+              <a href="<?= $pages->findByURI('projects')->url() ?>" >
                 <div id="stores" class="info-box">
                   Our Stores
                 </div>
@@ -46,13 +46,27 @@
       </div>
       <div class="row">
         <div class="col logo">
-            1
+            <img src="<?php echo $page->credentialOne()->image()->file()->url() ?>"> <?php echo $page->credentialOne()->url() ?>
         </div>
         <div class="col logo">
-            2
+          <?php if($image = $page->image()): ?>
+            <img src="<?php echo $image->url() ?>" alt="">
+          <?php endif ?>
         </div>
         <div class="col logo">
-            3
+          <ul class="teaser cf">
+            <?php foreach(page('projects')->children()->visible()->limit(3) as $project): ?>
+            <li>
+              <h3><a href="<?php echo $project->url() ?>"><?php echo $project->title()->html() ?></a></h3>
+              <p><?php echo $project->text()->excerpt(80) ?> <a href="<?php echo $project->url() ?>">read&nbsp;more&nbsp;→</a></p>
+              <?php if($image = $project->images()->sortBy('sort', 'asc')->first()): ?>
+              <a href="<?php echo $project->url() ?>">
+                <img src="<?php echo $image->url() ?>" alt="<?php echo $project->title()->html() ?>" >
+              </a>
+              <?php endif ?>
+            </li>
+            <?php endforeach ?>
+          </ul>
         </div>
         <div class="col logo">
             4
@@ -95,5 +109,7 @@
       </div>
 
     </section>
+
 <script src="//lightwidget.com/widgets/lightwidget.js"></script>
+
 <?php snippet('footer') ?>
