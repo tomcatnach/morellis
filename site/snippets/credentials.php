6ac8d4c6225db1ff76fdad94c8586028647df0f6
Tom <tom@studiographene.com>
2017-04-02 21:23:22 +0100

    Working who we work with section

---
+++ site/snippets/credentials.php
@@ -0,0 +1,29 @@
+<?php
+
+$credentials = page('credentials')->children();
+
+/*
+
+The $limit parameter can be passed to this snippet to
+display only a specified amount of credentials:
+
+```
+<?php snippet('showcase', ['limit' => 3]) ?>
+```
+
+Learn more about snippets and parameters at:
+https://getkirby.com/docs/templates/snippets
+
+*/
+
+if(isset($limit)) $credentials = $credentials->limit($limit);
+
+?>
+
+  <?php foreach($credentials as $credential): ?>
+    <?php if($credential->Credimage()->isNotEmpty()): ?>
+      <div class="col">
+        <img src="<?= $credential->Credimage()->toFile()->resize(150)->url() ?>" alt="" />
+      </div>
+    <?php endif ?>
+  <?php endforeach ?>
--- site/templates/home.php
+++ site/templates/home.php
@@ -40,50 +40,29 @@
 
     <div class="container" id="who-we-work-with">
       <div class="row">
-        <div class="title col">
-          Who we serve
-        </div>
+        <h2>Who we serve</h2>
       </div>
       <div class="row">
-        <div class="col logo">
-            <img src="<?php echo $page->credentialOne()->image()->file()->url() ?>"> <?php echo $page->credentialOne()->url() ?>
-        </div>
-        <div class="col logo">
-          <?php if($image = $page->image()): ?>
-            <img src="<?php echo $image->url() ?>" alt="">
-          <?php endif ?>
-        </div>
-        <div class="col logo">
-          <ul class="teaser cf">
-            <?php foreach(page('projects')->children()->visible()->limit(3) as $project): ?>
-            <li>
-              <h3><a href="<?php echo $project->url() ?>"><?php echo $project->title()->html() ?></a></h3>
-              <p><?php echo $project->text()->excerpt(80) ?> <a href="<?php echo $project->url() ?>">read&nbsp;more&nbsp;→</a></p>
-              <?php if($image = $project->images()->sortBy('sort', 'asc')->first()): ?>
-              <a href="<?php echo $project->url() ?>">
-                <img src="<?php echo $image->url() ?>" alt="<?php echo $project->title()->html() ?>" >
-              </a>
-              <?php endif ?>
-            </li>
-            <?php endforeach ?>
-          </ul>
-        </div>
-        <div class="col logo">
-            4
-        </div>
+        <?php snippet('credentials', ['limit' => 6]) ?>
       </div>
     </div>
 
     <div class="container media-row">
       <div class="row">
         <div class="instagram col">
-          <!-- LightWidget WIDGET --><iframe src="//lightwidget.com/widgets/384460a2c7aa5a0c99d2ecf9287937a9.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+          <!-- LightWidget WIDGET -->
+          <iframe src="//lightwidget.com/widgets/384460a2c7aa5a0c99d2ecf9287937a9.html" scrolling="no"
+          allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
         </div>
         <div class="instagram col">
-          <!-- LightWidget WIDGET --><iframe src="//lightwidget.com/widgets/19ba063eade751d9b5bddd31503d218d.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+          <!-- LightWidget WIDGET -->
+          <iframe src="//lightwidget.com/widgets/19ba063eade751d9b5bddd31503d218d.html" scrolling="no"
+          allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
         </div>
         <div class="instagram col">
-          <!-- LightWidget WIDGET --><iframe src="//lightwidget.com/widgets/d25a7d7dcc395965af15044926e56f43.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+          <!-- LightWidget WIDGET -->
+          <iframe src="//lightwidget.com/widgets/d25a7d7dcc395965af15044926e56f43.html" scrolling="no"
+          allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
         </div>
       </div>
     </div>
@@ -99,14 +78,6 @@
     <div class="text wrap">
       <?= $page->text()->kirbytext() ?>
     </div>
-
-    <section class="projects-section">
-
-      <div class="wrap wide">
-        <h2>Latest Projects</h2>
-        <?php snippet('showcase', ['limit' => 3]) ?>
-        <p class="projects-section-more"><a href="<?= page('projects')->url() ?>" class="btn">show all projects &hellip;</a></p>
-      </div>
 
     </section>
 
