8e2cdf3e41f66d299aa66ac0c21eff116ce442ce
tomcatnach <tom@notobl.com>
2017-08-28 22:34:53 +0100

    franchise update

--- site/templates/franchise.php
+++ site/templates/franchise.php
@@ -1,5 +1,25 @@
 <?php snippet('header') ?>
 
-
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-auto">
+      <h1><?= $page->title()->html() ?></h1>
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-8">
+      Sales content required<br><br><br>
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-auto">
+      <form method="POST" action="http://formspree.io/tom@notobl.com">
+        <input type="email" name="email" placeholder="Your email">
+        <textarea name="message" placeholder="Your message"></textarea>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  </div>
+</div>
 
 <?php snippet('footer') ?>
