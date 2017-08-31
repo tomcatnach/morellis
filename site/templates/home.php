4972819cd420a804e054ac611882916a72a9512e
tomcatnach <tom@notobl.com>
2017-08-31 21:48:54 +0100

    Homepage test fix on iPhone

--- site/templates/home.php
+++ site/templates/home.php
@@ -41,38 +41,39 @@
       </div>
     </div>
 
-    <div class="container" id="who-we-work-with">
-      <div class="row">
-        <div class="col title">
-          <h2>Who we serve</h2>
+    <div class="white-background">
+      <div class="container" id="who-we-work-with">
+        <div class="row">
+          <div class="col title">
+            <h2>Who we serve</h2>
+          </div>
+        </div>
+        <div class="row">
+          <?php snippet('credentials') ?>
         </div>
       </div>
-      <div class="row">
-        <?php snippet('credentials') ?>
-      </div>
-    </div>
 
-    <div class="container media-row">
-      <div class="row">
-        <div class="instagram col-4">
-          <!-- LightWidget WIDGET -->
-          <iframe src="//lightwidget.com/widgets/24d94ddc518357bbb2c481b7c064b569.html" scrolling="no"
-          allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
-        </div>
-        <div class="instagram col-4">
-          <!-- LightWidget WIDGET -->
-          <iframe src="//lightwidget.com/widgets/1d1ca7923e7a5762a4c25b24c9928f33.html" scrolling="no"
-          allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
-        </div>
-        <div class="instagram col-4">
-          <!-- LightWidget WIDGET -->
-          <iframe src="//lightwidget.com/widgets/2fdd688674b852ec9c99eef9aa131b5d.html" scrolling="no"
-          allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+      <div class="container media-row">
+        <div class="row">
+          <div class="instagram col-4">
+            <!-- LightWidget WIDGET -->
+            <iframe src="//lightwidget.com/widgets/24d94ddc518357bbb2c481b7c064b569.html" scrolling="no"
+            allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+          </div>
+          <div class="instagram col-4">
+            <!-- LightWidget WIDGET -->
+            <iframe src="//lightwidget.com/widgets/1d1ca7923e7a5762a4c25b24c9928f33.html" scrolling="no"
+            allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+          </div>
+          <div class="instagram col-4">
+            <!-- LightWidget WIDGET -->
+            <iframe src="//lightwidget.com/widgets/2fdd688674b852ec9c99eef9aa131b5d.html" scrolling="no"
+            allowtransparency="true" class="lightwidget-widget" style="width: 100%; border: 0; overflow: hidden;"></iframe>
+          </div>
         </div>
       </div>
+      <?php snippet('contact') ?>
     </div>
-
-<?php snippet('contact') ?>
 
 <script src="//lightwidget.com/widgets/lightwidget.js"></script>
 
